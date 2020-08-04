a562301807c90ec939b7e20e78dadad47d359819
Tom Hatcher <tom.hatcher@myob.com>
2020-08-04 14:05:18 +1200

    Initial commit after creation of project

---
+++ PayslipKata001/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PayslipKata001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ PayslipUnitTests/UnitTest1.cs
@@ -0,0 +1,23 @@
+using System;
+using Xunit;
+
+namespace PayslipUnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+
+        [Theory]
+        [InlineData()]
+        [InlineData()]
+        public void ShouldWorkCorrectly()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+    }
+}
