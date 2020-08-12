bd2fb0c393b428e9777059710ffa525469646177
Tom Hatcher <tom.hatcher@myob.com>
2020-08-12 13:07:02 +1200

    Added superannuation calculator, tax calculator.

--- PayslipKata001/Superannuation.cs
+++ PayslipKata001/Superannuation.cs
@@ -1,7 +1,13 @@
+using System;
+
 namespace PayslipKata001
 {
     public class Superannuation
     {
-        
+        public static int Calculator( double netIncome, double superannuationRate)
+        {
+            var superannuationValue = (Math.Round(netIncome * superannuationRate / 100, MidpointRounding.AwayFromZero));
+            return Convert.ToInt32(superannuationValue);
+        }
     }
 }
--- PayslipKata001/Tax.cs
+++ PayslipKata001/Tax.cs
@@ -1,7 +1,15 @@
+using System;
+
 namespace PayslipKata001
 {
     public class Tax
     {
-        
+        public static int Calculator(double previousTaxAmount, double grossIncome, double previousTaxBracketLimit,
+            double taxPercentage, double payPeriodsPerYear)
+        {
+            var taxValue = (previousTaxAmount + (grossIncome - previousTaxBracketLimit) * taxPercentage / 100) / 12;
+            var taxAmount = Math.Round(taxValue, MidpointRounding.AwayFromZero);
+            return Convert.ToInt32(taxAmount);
+        }
     }
 }
