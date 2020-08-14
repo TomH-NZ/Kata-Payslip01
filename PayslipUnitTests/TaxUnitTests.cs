0cb9a2d1b4ed88b0953be71e2b0f27fdfcbd5cc7
Tom Hatcher <tom.hatcher@myob.com>
2020-08-14 15:18:54 +1200

    Refactored Tax calculator, added working Fact and Theory tests for Tax unit test

--- PayslipUnitTests/TaxUnitTests.cs
+++ PayslipUnitTests/TaxUnitTests.cs
@@ -10,20 +10,26 @@ namespace PayslipUnitTests
         public void ShouldCalculateCorrectly()
         {
             //Arrange
+            var calculatedTax = Tax.Calculator(3572, 60050, 37000, 32.5, 12);
             //Act
             //Assert
+            Assert.Equal(922, calculatedTax);
         }
 
         [Theory]
+        [InlineData(3572, 60050, 37000, 32.5, 12, 922)]
+        /*[InlineData()]
         [InlineData()]
-        [InlineData()]
-        [InlineData()]
-        [InlineData()]
-        public void ShouldCalculateTaxCorrectly()
+        [InlineData()]*/
+        public void ShouldCalculateTaxCorrectly(double previousTaxAmount, double grossIncome, double previousTaxBracketLimit,
+            double taxPercentage, double payPeriodsPerYear, int outputTaxValue)
         {
             //Arrange
+            var taxCalculated = Tax.Calculator(previousTaxAmount, grossIncome, previousTaxBracketLimit, taxPercentage,
+                payPeriodsPerYear);
             //Act
             //Assert
+            Assert.Equal(outputTaxValue, taxCalculated);
         }
     }
 }
