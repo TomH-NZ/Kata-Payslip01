963279dac4c417c0a768e6aa835d3e6bab03ce06
Tom Hatcher <tom.hatcher@myob.com>
2020-08-16 10:48:19 +1200

    Started payslip console display

--- PayslipKata001/Program.cs
+++ PayslipKata001/Program.cs
@@ -6,7 +6,19 @@ namespace PayslipKata001
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the payslip generator. \n");
+            Console.WriteLine("Please enter your name:");
+            var firstName = Console.ReadLine();
+            Console.WriteLine("Please enter your surname:");
+            var lastName = Console.ReadLine();
+            Console.WriteLine("Please enter your annual salary:");
+            var grossSalary = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter your superannuation rate:");
+            var superannuationRate = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter the pay period start date:");
+            var startDate = Console.ReadLine();
+            Console.WriteLine("Please enter the pay period end date:");
+            var endDate = Console.ReadLine();
         }
     }
 }
