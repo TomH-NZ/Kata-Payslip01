acd49c474b94a2c8dfbfc293f65eac8cb91e306d
Tom Hatcher <tom.hatcher@myob.com>
2020-08-18 13:42:26 +1200

    zAdded comment for future refactoring on Payslip class

--- PayslipKata001/Payslip.cs
+++ PayslipKata001/Payslip.cs
@@ -4,6 +4,8 @@ namespace PayslipKata001
 {
     public class Payslip
     {
+        //Possible to move all code from Program to here, call as new object?
         
+        // payslip TomH = new Payslip(); which would run all of the console commands, etc.
     }
 }
